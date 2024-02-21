7be5f2942e17f7695a915968d8c9bde643a8c151
Beste Çeltek <besteceltek@gmail.com>
2024-02-21 23:31:27 +0300

    Added comments

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Defined variables
         int year;
         String zodiac = null;
 
+        //Prompted the year of birth from user
         Scanner input = new Scanner(System.in);
         System.out.print("Please enter your birth year: ");
         year = input.nextInt();
 
+        //Calculated and determined the Chinese zodiac according to year of birth
         zodiac = switch (year % 12) {
             case 0 -> "Monkey";
             case 1 -> "Rooster";
